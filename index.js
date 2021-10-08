926d8ec20a9d9eb143bd2e61a925c2a1b904eb27
Khaliddxx <doodz@aucegypt.edu>
2021-10-09 01:41:31 +0200

    fixed start script

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ env.config();
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
-
 const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
